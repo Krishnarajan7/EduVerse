a3a7f66a30eed67122c4b35c95a6d67de413ef01
krishnarajan <krishh.v777@gmail.com>
2025-03-23 22:46:51 +0530

    add db to settings and register the app URL path

--- eduverse/settings.py
+++ eduverse/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'eduverse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'eduverse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eduverse_db',
+        'USER': 'postgres',
+        'PASSWORD':'krishxop007',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
