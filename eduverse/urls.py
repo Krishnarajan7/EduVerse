a85bf68d8676c085e83b54e1a738d7056a356405
krishnarajan <krishh.v777@gmail.com>
2025-03-24 22:51:05 +0530

    Fix students route data and add basic home route

--- eduverse/urls.py
+++ eduverse/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from django.contrib import admin
+from django.http import HttpResponse  
+
+def home(request):
+    return HttpResponse("Welcome to EduVerse ERP!")
 
 urlpatterns = [
+    path('', home, name='home'), 
     path('students/', include('students.urls')),
+    path('admin/', admin.site.urls),
 ]
