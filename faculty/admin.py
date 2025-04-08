921d59d56657b02805e7445202b5e1e3db5eb54e
krishnarajan <krishh.v777@gmail.com>
2025-04-08 14:47:06 +0530

    update admin for Student and Faculty based on their Model

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Faculty
+from .models import Faculty,Notice,Resource
 
 admin.site.register(Faculty)
+admin.site.register(Notice)
+admin.site.register(Resource)
 
--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Student,Class
+from .models import Student,Class,Subject,Attendance,Fee
 
 admin.site.register(Student)
 admin.site.register(Class)
+admin.site.register(Subject)
+admin.site.register(Attendance)
+admin.site.register(Fee)
