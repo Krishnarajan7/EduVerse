29b0885a8547939b87384d01ce8c951afa518816
krishnarajan <krishh.v777@gmail.com>
2025-04-28 12:58:13 +0530

    set frontend using vite latest version

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+function ProfilePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Profile Page</h1>
+      <p className="mt-2">Student details will go here.</p>
+    </div>
+  );
+}
+export default ProfilePage;
