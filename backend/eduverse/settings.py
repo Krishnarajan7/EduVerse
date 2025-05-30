715136bb68ee66863a67dffb804298e3f1950ab6
Krishnarajan <krishh.v777@gmail.com>
2025-05-30 22:33:23 +0530

    update to prevent sensitive data

--- backend/eduverse/settings.py
+++ backend/eduverse/settings.py
@@ -10,7 +10,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-*mmak5i(+27#-b1@+l!y=wnux*)6-#%mm0$59))rb^w&e44(4$')
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-*mmak5i(+27#-b1@+l!y=wnux*)6-#%mm0$59))rb^w&e44(4$')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
